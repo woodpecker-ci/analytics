6203e171ee530e3b3e2d760a8185c476ae90c88a
Anbraten <anton@ju60.de>
2022-09-05 08:13:10 +0000

    :tada: init

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/influxdata/influxdb-client-go/v2"
+)
+
+func main() {
+	client := influxdb2.NewClient("http://localhost:8086", "my-token")
+	writeAPI := client.WriteAPIBlocking("woodpecker", "analytics")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/api/v1/meta", func(w http.ResponseWriter, r *http.Request) {
+		p := influxdb2.NewPointWithMeasurement("stat").
+			AddTag("type", "meta").
+			AddField("version", 123).
+			AddField("repos", 200).
+			AddField("users", 3).
+			AddField("pipelines", 123456).
+			AddField("pipelines_time", 4321).
+			AddField("forge", "gitea").
+			AddField("server_os", "linux/amd64").
+			AddField("agents", 1).
+			AddField("agent_backend", "123").
+			SetTime(time.Now())
+		writeAPI.WritePoint(context.Background(), p)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	client.Close()
+}
